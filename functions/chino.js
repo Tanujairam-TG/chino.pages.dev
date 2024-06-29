96b6980b4f48aa82e67f830801833c41c7b698bd
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-06-29 07:29:59 +0530

    Update chino.js

--- functions/chino.js
+++ functions/chino.js
@@ -1,6 +1,6 @@
 export async function onRequestGet() {
     const max = 267;
-    return new Response(await fetch(`https://raw.githubusercontent.com/AutumnVN/chino.webp/main/${Math.ceil(Math.random() * max)}.webp`).then(r => r.blob()), {
+    return new Response(await fetch(`https://raw.githubusercontent.com/Tanujairam-TG/chino.webp/main/${Math.ceil(Math.random() * max)}.webp`).then(r => r.blob()), {
         headers: {
             'content-type': 'image/webp',
             'cache-control': 'no-cache, no-store, must-revalidate, max-age=0'
